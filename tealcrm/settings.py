fca67088af35b2291261982ff258c834a8083a28
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-27 12:21:44 +0100

    base template

--- tealcrm/settings.py
+++ tealcrm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "Django_CRM_App.urls"
+ROOT_URLCONF = "tealcrm.urls"
 
 TEMPLATES = [
     {
