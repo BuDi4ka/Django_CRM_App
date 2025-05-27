f5945fdad6b35ae605ebd8c1c8d78bb2591a0c87
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-27 16:20:43 +0100

    Add leads list view and template, update navigation link

--- lead/urls.py
+++ lead/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-lead', views.add_lead, name='add-lead')
+    path('', views.leads_list, name='leads_list'),
+    path('add-lead', views.add_lead, name='add-lead'),
 ]
