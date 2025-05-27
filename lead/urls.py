ec605479c89800eb89e9c97252c8b6b23a9c27bd
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-27 16:40:53 +0100

    Refactor URL naming for leads and update leads detail view with a back link

--- lead/urls.py
+++ lead/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.leads_list, name='leads_list'),
+    path('', views.leads_list, name='leads-list'),
     path('<int:pk>/', views.leads_detail, name='leads-detail'),
     path('add-lead', views.add_lead, name='add-lead'),
 ]
