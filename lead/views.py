f666b61a38e52e23e5bd535f7e1e15b42b43e369
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-28 16:12:02 +0100

    Refactor lead views and templates to use class-based views; update delete functionality with CSRF protection

--- lead/views.py
+++ lead/views.py
@@ -1,6 +1,10 @@
 from django.contrib import messages
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
+from django.views.generic.edit import DeleteView
 
 
 from .forms import AddLeadForm
@@ -10,54 +14,93 @@ from client.models import Client
 from team.models import Team
 
 
-@login_required
-def leads_list(request):
-    leads = Lead.objects.filter(created_by=request.user, converted_to_client=False)
+class LeadListView(ListView):
+    model = Lead
+    template_name = 'lead/leads_list.html'
+    context_object_name = 'leads'
 
-    return render(request, 'lead/leads_list.html', {
-        'leads': leads
-    })
-
-
-@login_required
-def leads_detail(request, pk):
-    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
-
-    return render(request, 'lead/leads_detail.html', {
-        'lead': lead
-    })
-
-
-@login_required
-def leads_delete(request, pk):
-    lead = get_object_or_404(Lead, pk=pk)
-    lead.delete()
-
-    messages.success(request, 'The lead was deleted.')
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
     
-    return redirect('leads:list')
+    def get_queryset(self):
+        queryset = super(LeadListView, self).get_queryset()
+        queryset = queryset.filter(created_by=self.request.user, converted_to_client=False)
+
+        return queryset
+
+    
+class LeadDetailView(DetailView):
+    model = Lead
+    context_object_name = 'lead'
+    template_name = 'lead/leads_detail.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+
+    def get_queryset(self):
+        queryset = super(LeadDetailView, self).get_queryset()
+
+        return queryset.filter(created_by=self.request.user, pk=self.kwargs.get('pk'))
 
 
-@login_required
-def leads_edit(request, pk):
-    lead = get_object_or_404(Lead, pk=pk)
+class LeadDeleteView(DeleteView):
+    model = Lead
+    success_url = reverse_lazy('leads:list')
 
-    if request.method == "POST":
-        form = AddLeadForm(request.POST, instance=lead)
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+    
+    def get_template_names(self):
+        return []   
 
-        if form.is_valid():
-            form.save()
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
 
-            messages.success(request, 'The changes was saved')
+    def get_queryset(self):
+        queryset = super(LeadDeleteView, self).get_queryset()
 
-            return redirect('leads:list')
-    else:
-        form = AddLeadForm(instance=lead)
+        return queryset.filter(created_by=self.request.user, pk=self.kwargs.get('pk'))
 
-    return render(request, 'lead/leads_edit.html', {
-        'form': form,
-        'lead': lead
-    })
+
+class LeadUpdateView(UpdateView):
+    model = Lead
+    fields = ('name', 'email', 'description', 'priority', 'status')
+    template_name = 'lead/leads_edit.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get_queryset(self):
+        queryset = super(LeadUpdateView, self).get_queryset()
+
+        return queryset.filter(created_by=self.request.user, pk=self.kwargs.get('pk'))
+    
+    def get_success_url(self):
+        return reverse_lazy('leads:list')
+
+
+class LeadCreateView(CreateView):
+    model = Lead
+    fields = ('name', 'email', 'description', 'priority', 'status')
+    template_name = 'lead/leads_add.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get_success_url(self):
+        return reverse_lazy('leads:list')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["team"] = Team.objects.filter(created_by=self.request.user)[0]
+        return context
 
 
 
