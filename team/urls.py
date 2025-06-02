e7c7d8d5274b33c2a5ce682b5ce3b6fa59bc0551
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-02 11:36:35 +0100

    Add activate team functionality; update URLs and views to allow users to set an active team, and modify template to link to the new activation route.

--- team/urls.py
+++ team/urls.py
@@ -7,5 +7,6 @@ app_name = 'team'
 urlpatterns = [
     path('', views.teams_list, name='list'),
     path('<int:pk>/edit/', views.edit_team, name='edit'),
+    path('<int:pk>/activate/', views.activate_team, name='activate'),
     path('<int:pk>/detail/', views.detail, name='detail'),
 ]
