2eea754aecb4d216a5133cf9aa248ff3592332e3
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-02 12:28:00 +0100

    Update Tailwind CSS styles for team detail template; enhance layout and button styles for better user experience. Fix user profile reference in dashboard view and add new HTML content paths in Tailwind config. Update SQLite database.

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,7 +8,7 @@ from team.models import Team
 
 @login_required
 def dashboard(request):
-    team = team = request.user.useprofile.active_team
+    team = team = request.user.userprofile.active_team
 
     leads = Lead.objects.filter(team=team, converted_to_client=False).order_by('-created_at')[:5]
     clients = Client.objects.filter(team=team).order_by('-created_at')[:5]
